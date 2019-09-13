6da5156da8851762ffec99e67ef64289f7b191db
Daniel Bolivar <Daniel.bolivar.freelance@gmail.com>
2019-09-13 17:30:21 -0500

    arrastrar, transiciones, y zoom incluido. falta perfeccionar, crear nuevas funciones y convertir en clases para facil implementacion, ver si funciona con muchas imagenes

--- bold.js
+++ bold.js
@@ -11,45 +11,73 @@ function main(){
 PARAMETROS PARA SLIDER.
 	CONTENT-SLIDE = ID DEL CONTENEDOR DE IMAGENES.
 	RANGO DE DESPLAZAMIENTO: CANTIDA DE PIXELES A DESPLAZAR PARA CAMBIAR UNA IMAGEN
+	PROBAR SLIDER CON MULTIPLES FOTOS Y CON DOS FOTOS.
 */
 
-
-
-
-
 function initSlide(id,range, zoom){
-	cord = document.getElementById('cord');
-	ctx = document.getElementById(id);
+	// cord = document.getElementById('cord');
+	ctx = document.getElementById(id);							// CONTENEDOR ASIGNADO PARA EL SLIDER.
+	// agrego indices a las imagenes.
+	imagesSlide = ctx.querySelectorAll('.item-image');
+	imagesSlide.forEach((item,index)=>{
+		item.dataset.id_img= index;
+	})
+	totalImages = imagesSlide.length-1;									// CANTIDAD DE IMAGENES DEL SLIDER
+	lastImage  	= 0;											// ULTIMO INDICE DE IMAGEN QUE SE MOSTRO.
+	bod = document.getElementsByTagName('body')[0];				// CUERPO - BODY HTML
 	posx = 0;
 	posy = 0;
-
+	img = "undefined";
+	url = window.location.href;
+	baseUrl = url.split('/').slice(0,length-1).join('/');		// URL BASE DE LA PAGINA PARA OBTENER LAS RUTA.
+	image = null;
 	if(zoom){
+		img = document.createElement('img');
 		d = document.createElement('div');
 		d.className = "zoomin noview";
 		ctx.appendChild(d);
+		zoomSection = document.createElement('div');
+		zoomSection.className = "zoom-section noview";
+		bod.appendChild(zoomSection);
 		zoomIn();
 	}
 	ctx.onmousedown = function(e){
 		xInit = e.clientX;
 		yInit = e.clientY;
 		d.classList.add('noview');
-		document.onmousemove = function(e){
-			cord.innerText = "coord: x="+e.clientX+" y="+e.clientY;
-			if(xInit < e.clientX){
+		ctx.onmousemove = function(e){
+			d.classList.add('noview');
+			// cord.innerText = "coord: x="+e.clientX+" y="+e.clientY;
+			if(xInit < e.clientX){ // IMAGEN ANTERIOR
 				xSecundario = xInit+ range;
 				if(xSecundario < e.clientX){
 					console.log('move to RIGHT');
+					previousImg = ctx.querySelector('.selected-slide');
+					// console.log(previousImg);
+					index = parseInt(previousImg.dataset.id_img)-1;
+					if(index<0){
+						index = totalImages;
+					}
+					changeImg(index,previousImg);
 				}
-			}else{
+			}else{ // IMAGEN POSTERIOR
 				xSecundario = xInit -range;
 				if(xSecundario > e.clientX){
 					console.log('move to LEFT');
+					previousImg = ctx.querySelector('.selected-slide');
+					console.log(previousImg);
+					index = parseInt(previousImg.dataset.id_img)+1;
+					console.log('indice',index);
+					if(index>totalImages){
+						index = 0;
+					}
+					changeImg(index,previousImg);
 				}
 			}
 		}
-		document.onmouseup = function(e){
+		ctx.onmouseup = function(e){
 			cord.innerText = "arrastre la imagen hacia los lados";
-			document.onmousemove = function(){
+			ctx.onmousemove = function(){
 				if(zoom){
 					return zoomIn();
 				}
@@ -59,21 +87,142 @@ function initSlide(id,range, zoom){
 }
 
 function zoomIn(){
+	let el = ctx.querySelector('.selected-slide');
+	image = baseUrl+"/"+el.getAttribute('image-zoom');
+	// console.log(image);
+	if(typeof(img) == "undefined"){
+		console.log('creando img');
+		// img = document.createElement('img');
+		img = new Image();
+		if(img.src != image){
+			console.log('src');
+			// img.src =image;
+			img.src = image;
+			img.onload = imgLoad;
 
+			// imgWidth = img.width;	// ancho imagen cargada
+			// imgHeight = img.height;	// alto imagen cargada
+		}
+	}else{
+		if(img.src != image){
+			console.log('src');
+			img.src = image;
+			img.onload = imgLoad;
+			// imgWidth = img.width;	// ancho imagen cargada
+			// imgHeight = img.height;	// alto imagen cargada
+		}
+		console.log('coor');
+		imgLoad();
+	}
+	// imgWidth = img.width;	// ancho imagen cargada
+	// imgHeight = img.height;	// alto imagen cargada
+	// console.log('ejecutado asigno imagen');
+	// zoomSection.style.background = "url("+image+")";
+	// ctx.onmousemove = function(e){
+	// 	console.log(e.clientX);
+	// 	zoomSection.classList.remove('noview');
+	// 	// zoomSection.style.background = "url("+image+")";
+
+	// 	// posicion puntero image
+	// 	pleft 	= e.clientX-this.offsetLeft;
+	// 	ptop	= e.clientY-this.offsetTop;
+	// 	console.log(pleft,ptop);
+	// 	//razones de desplazamiento.
+	// 	console.log(imgWidth,imgHeight)
+	// 	rleft = pleft*imgWidth/el.width;
+	// 	rtop  = ptop*imgHeight/el.height;
+
+	// 	console.log(rleft,rtop);
+	// 	leftzoom =rleft-150;
+	// 	topzoom = rtop -150; // 150 tama;o de la caja
+	// 	console.log(leftzoom,topzoom);
+	// 	if(leftzoom <= 0){
+	// 		leftzoom =0;
+	// 	}
+	// 	if(topzoom <= 0){
+	// 		topzoom =0;
+	// 	}
+	// 	zoomSection.style.backgroundPosition = "-"+leftzoom+"px -"+topzoom+"px";
+	// }
+	// ctx.onmouseleave = function(e){
+	// 	d.classList.add('noview');
+	// 	zoomSection.classList.add('noview');
+	// 	ctx.onmousemove = function(){
+	// 		console.log(222);
+	// 		zoomIn();
+	// 	};
+	// }
+}
+
+function imgLoad(){
+	el = ctx.querySelector('.selected-slide');
+	imgWidth = img.width;	// ancho imagen cargada
+	imgHeight = img.height;	// alto imagen cargada
+	console.log('ejecutado asigno imagen');
+	zoomSection.style.background = "url("+image+")";
 	ctx.onmousemove = function(e){
-		// console.log(e);
-		d.classList.remove('noview');
-		posx = e.offsetX;
-		posy = e.offsetY;
-		d.style.left = posx+"px";
-		d.style.top = posy+window.scrollY+"px";
+		// console.log(e.clientX);
+		zoomSection.classList.remove('noview');
+		// zoomSection.style.background = "url("+image+")";
+
+		// posicion puntero image
+		pleft 	= e.clientX-this.offsetLeft;
+		ptop	= e.clientY-this.offsetTop;
+		// console.log(pleft,ptop);
+		//razones de desplazamiento.
+		// console.log(imgWidth,imgHeight)
+		rleft = pleft*imgWidth/el.width;
+		rtop  = ptop*imgHeight/el.height;
+
+		// console.log(rleft,rtop);
+		leftzoom =rleft-150;
+		topzoom = rtop -150; // 150 tama;o de la caja
+		// console.log(leftzoom,topzoom);
+		if(leftzoom <= 0){
+			leftzoom =0;
+		}
+		if(topzoom <= 0){
+			topzoom =0;
+		}
+		zoomSection.style.backgroundPosition = "-"+leftzoom+"px -"+topzoom+"px";
 	}
 	ctx.onmouseleave = function(e){
-		posx = e.clientX;
-		posy = e.clientY;
-		console.log(d,'afuera');
 		d.classList.add('noview');
+		zoomSection.classList.add('noview');
+		ctx.onmousemove = function(){
+			console.log(222);
+			zoomIn();
+		};
 	}
+}
+
+
+
+
+
+function changeImg(index,previousImg){
+	console.log(index);
+	console.log(lastImage);
+	// DEFINO QUE TRANSICION SE USARA.
+	if(index < lastImage){
+		nameTransitionNewImage = 'move-right-new';
+		nameTransitionPreviousImage = 'move-right-previous';
+	}else{
+		nameTransitionNewImage = 'move-left-new';
+		nameTransitionPreviousImage = 'move-left-previous';
+		lastImage = index;
+	}
+	lastImage = index;
+	//quito la imagen previa
+	previousImg.classList.remove('selected-slide');
+	previousImg.style.animationName = nameTransitionPreviousImage;
+	//agrego la nueva imagen
+	imagesSlide[index].classList.add('selected-slide');
+	imagesSlide[index].style.animationName = nameTransitionNewImage;
+	ctx.onmousemove = function(e){
+		return zoomIn();
+	}
+
 }
 
 
